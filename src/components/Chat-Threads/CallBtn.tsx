705b58fe1ba3a6090674f32954a7fc08e45409c3
ghulammujtaba153 <ghulammujtaba.dro@gmail.com>
2024-01-29 13:48:40 +0500

    Commit changes in the client submodule

--- src/components/Chat-Threads/CallBtn.tsx
+++ src/components/Chat-Threads/CallBtn.tsx
@@ -1,10 +1,33 @@
-import { PhoneIcon } from '@/utils/icons'
-import React from 'react'
+"use client"
 
-const CallBtn = () => {
+import { useSelectedUser, useUser } from '@/store/useStore';
+import { PhoneIcon } from '@/utils/icons'
+import { useRouter } from 'next/navigation'
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import { io } from "socket.io-client";
+
+function CallBtn() {
+  const router = useRouter();
+  const socket = io("http://localhost:4000");
+  const [cookie] = useCookies(["user"]);
+  const selectedUser = useSelectedUser((state) => state.selectedUser);
+  const myUser = useUser((state) => state.myUser);
+
+  function handleClick() {
+    socket.emit(
+      "private message",
+      selectedUser.email,
+      "📞" + myUser.name + " is calling " + selectedUser.name + "📞",
+      cookie.user
+    )
+    router.push("/chat/room");
+  }
+
+
   return (
-    <button>
-        <PhoneIcon/>
+      <button onClick={handleClick}>
+          <PhoneIcon/>
     </button>
   )
 }
